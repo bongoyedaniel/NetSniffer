252696776ca32dd3e95666b0fdc428921f7d39ef
bongoye <95305237+bongoyedaniel@users.noreply.github.com>
2024-01-30 13:55:43 +0300

    Updated file

--- sample.java
+++ sample.java
@@ -1,59 +1,31 @@
-// This software is for intended for network auditing.
-// Copyright (C) 2014 Vittus Peter Ove Maqe Mikiassen
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-// for capturing
 import jpcap.JpcapCaptor;
 import jpcap.JpcapSender;
 import jpcap.NetworkInterfaceAddress;
 import jpcap.NetworkInterface;
 import jpcap.PacketReceiver;
-
-// just to include the packets required to sniff..
 import jpcap.packet.Packet;
 import jpcap.packet.ARPPacket;
 import jpcap.packet.ICMPPacket;
-
-// for charsetdeocding of packet
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.charset.CharsetDecoder;
 import java.nio.ByteBuffer;
-
-// for reading packets
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-// for writing packets
 import java.io.FileWriter;
-
-// use if necessary
 import java.io.IOException;
 
 public class sample {
 
     public static void main(String args[]) throws Exception {
-
-	// open wifisniffer
 	int index = 2;
 	NetworkInterface[] devices = JpcapCaptor.getDeviceList();
 	JpcapCaptor captor = JpcapCaptor.openDevice(devices[index], 4096, false, 5000);
 
-	// for decoding bytes captured
+	// decoding bytes captured
 	
 	ByteBuffer packbbheader = null;
 	ByteBuffer packbbdata = null;
